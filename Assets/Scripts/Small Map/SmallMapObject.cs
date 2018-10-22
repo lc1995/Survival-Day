095b10102df85f4c1b2692a3e8c567eeb438a80c
CHANG <lucianolin1995@gmail.com>
2018-10-22 09:46:04 +0200

    为僵尸添加了基本动画效果

--- Assets/Scripts/Small Map/SmallMapObject.cs
+++ Assets/Scripts/Small Map/SmallMapObject.cs
@@ -8,6 +8,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D)), RequireComponent(typeof(Animator))]
 public class SmallMapObject : MonoBehaviour {
 
     // ------ Public Variables ------
@@ -23,15 +24,25 @@ public class SmallMapObject : MonoBehaviour {
 
     // ------ Required Components ------
     private Rigidbody2D rb2d;
+    private Animator animator;
 
     // ------ Event Functions ------
     void Start () {
 		rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 	}
 
     void Update () {
+        Vector2 direction = ai.GetDirection(transform, PlayerControl.instance.transform);
         if(rb2d.bodyType == RigidbodyType2D.Dynamic)
-		    GetComponent<Rigidbody2D>().position += ai.GetDirection(transform, PlayerControl.instance.transform) * speed * Time.deltaTime;
+		    GetComponent<Rigidbody2D>().position += direction * speed * Time.deltaTime;
+
+        // Update animation
+        if(direction != Vector2.zero){
+            animator.SetBool("IsMoving", true);
+        }else{
+            animator.SetBool("IsMoving", false);
+        }       
 	}
 
     void OnTriggerEnter2D(Collider2D other){    
